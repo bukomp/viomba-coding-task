20b61a9caa71de5b3e743c0438d49964ce709e30
Edvard <edvard@shalaev.com>
2023-09-20 18:55:04 +0300

    feat: included dist to repo  & updated readme

--- README.md
+++ README.md
@@ -31,3 +31,60 @@ Light
 
 ## Solution
 
+### Key Decisions
+
+- **Ignore Initial Divs**: The code is specifically designed to disregard any `div` elements with a `role` attribute set to `"note"` that appear before the first paragraph. This ensures that these elements, which are likely not primary content, do not interfere with the main purpose of the application.
+
+- **URI Decoding**: All the links fetched have their URI encoded characters decoded back to their original form. This ensures that links are processed in their natural, readable form, especially when containing special characters.
+
+- **Cyclic Link Prevention**: To avoid any potential infinite loops and make the output concise, cyclic links are not included in the final tree structure. A `visited` set is used to keep track of the articles that have already been processed.
+
+**Note**: While creating this solution, I aimed to adhere to the given instructions and also applied logical reasoning to make certain decisions that would improve the program's functionality and user experience.
+
+
+### Key Information
+
+- **Main Entry Point:** `index.js`
+- **Commands:** 
+  - `npm test` - It appears no test has been specified for this project yet, as the command will throw an error.
+  - `npm run build` - Compiles the TypeScript code using the TypeScript compiler.
+- **Dependencies**:
+  - `axios`: A popular HTTP client used for making requests.
+  - `cheerio`: A fast, flexible, and lean implementation of core jQuery designed specifically for the server.
+### Code Explanation
+
+1. **Imports**: 
+   - `axios` is used for fetching the content of the articles.
+   - `cheerio` is used for parsing the fetched HTML data.
+
+2. **Function: `getLinks`**: 
+   - It's an asynchronous function that recursively fetches links from a Wikipedia article.
+   - The depth specifies how deep the function should go when following links.
+   - Limit determines how many links to fetch per article.
+   - `visited` set keeps track of already visited articles to avoid cyclic reference.
+   - The function fetches data from Wikipedia, then uses cheerio to parse the links from the article content.
+
+3. **Function: `main`**: 
+   - It's the main function that acts as the entry point.
+   - Takes command line arguments to determine the starting article, depth, and limit.
+   - If the provided arguments are not valid, the function outputs a usage hint.
+   - Calls `getLinks` with the provided arguments.
+
+4. **Execution**: 
+   - The `main` function is invoked at the end. Any errors are caught and logged to the console.
+
+### Setup:
+
+Before running the application, make sure to:
+
+1. Install all the required dependencies using `npm install`.
+2. Transpile the TypeScript code using the provided build script: `npm run build`.
+### Usage
+
+To use the application, run it with Node providing three command-line arguments: the article name, depth, and limit. For example:
+
+```shell
+npm start "Node.js" 2 5
+```
+
+This will fetch links related to "Node.js" on Wikipedia, with a depth of 2 and a limit of 5 links per article.
---
+++ dist/index.js
@@ -0,0 +1,76 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const cheerio = __importStar(require("cheerio"));
+const getLinks = (article, depth, limit, indentation = '', visited = new Set()) => __awaiter(void 0, void 0, void 0, function* () {
+    if (depth < 0)
+        return;
+    visited.add(article);
+    const { data } = yield axios_1.default.get(`https://en.wikipedia.org/wiki/${article}`);
+    const $ = cheerio.load(data);
+    const links = [];
+    $('#mw-content-text p').first().prevAll('div[role="note"]').remove();
+    $('#mw-content-text a').each((index, element) => {
+        const link = $(element).attr('href');
+        if (link &&
+            link.startsWith('/wiki/') &&
+            !link.includes(':') &&
+            links.length < limit &&
+            !visited.has(decodeURIComponent(link.slice(6)))) {
+            const decodedLink = decodeURIComponent(link.slice(6));
+            links.push(decodedLink);
+        }
+    });
+    console.log(`${indentation}${article}`);
+    for (const link of links) {
+        if (!visited.has(link)) {
+            yield getLinks(link, depth - 1, limit, `${indentation}  `, new Set(visited));
+        }
+    }
+});
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const article = process.argv[2];
+    const depth = parseInt(process.argv[3]);
+    const limit = parseInt(process.argv[4]);
+    if (!article || isNaN(depth) || isNaN(limit)) {
+        console.log('Usage: node src/index.ts [article] [depth] [limit]');
+        process.exit(1);
+    }
+    yield getLinks(article, depth, limit);
+});
+main().catch(console.error);
