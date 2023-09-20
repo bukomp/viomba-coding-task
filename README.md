de184aa36a25388d52c621a6b43725a406147799
Edvard <edvard@shalaev.com>
2023-09-20 18:12:52 +0300

    initial working version

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # viomba-coding-task
+
+## Challenge
+
+Write a programme which outputs into the console an intended tree structure listing of articles that are linked to a chosen article by following the links in the article.
+
+Input:
+	- The name of the English Wikipedia article where to start, for example, "Light"
+	- The desired depth how deep the links are followed, for example "2"
+	- How many first links are followed in each article, for example "3"
+
+Example output for the above values:
+
+``` 
+> node .\index.mjs Light 2 3
+
+Light
+  Electromagnetic_radiation
+    Linear_polarization
+    Physics
+    Electromagnetic_field
+  Dispersive_prism
+    Fused_quartz
+    Ultraviolet
+    Infrared
+  Dispersion_(optics)
+    Linear_response_function
+    Electric_susceptibility
+    Electromagnetic_spectrum#Visible_radiation_(light)
+```
+
+## Solution
+
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+const getLinks = async (
+  article: string,
+  depth: number,
+  limit: number,
+  indentation = ''
+): Promise<void> => {
+  if (depth < 0) return;
+
+  const { data } = await axios.get(`https://en.wikipedia.org/wiki/${article}`);
+  const $ = cheerio.load(data);
+  const links: string[] = [];
+
+  // Ignore divs with role "note" before the first p element
+  $('#mw-content-text p').first().prevAll('div[role="note"]').remove();
+  $('#mw-content-text a').each((index, element) => {
+    const link = $(element).attr('href');
+    if (
+      link &&
+      link.startsWith('/wiki/') &&
+      !link.includes(':') &&
+      links.length < limit
+    ) {
+      const decodedLink = decodeURIComponent(link.slice(6));
+      links.push(decodedLink);
+    }
+  });
+
+  console.log(`${indentation}${article}`);
+  for (const link of links) {
+    await getLinks(link, depth - 1, limit, `${indentation}  `);
+  }
+};
+
+const main = async () => {
+  const article = process.argv[2];
+  const depth = parseInt(process.argv[3]);
+  const limit = parseInt(process.argv[4]);
+
+  if (!article || isNaN(depth) || isNaN(limit)) {
+    console.log('Usage: node src/index.ts [article] [depth] [limit]');
+    process.exit(1);
+  }
+
+  await getLinks(article, depth, limit);
+};
+
+main().catch(console.error);
